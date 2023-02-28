7389138d37b3be799306365f4558eccaf349deb7
safiullahrafeeq77777 <safiullahrafeeq7777@gmail.com>
2023-02-28 13:44:27 +0500

    changes

--- src/component/FeeCard/index.jsx
+++ src/component/FeeCard/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import remitly from '../../assets/images/remitly.png'
-import western from '../../assets/images/western.png'
+import remitly from "../../assets/images/remitly.png";
+import western from "../../assets/images/western.png";
 const FeeCard = () => {
   return (
-    <div className="bg-[#C7FA50]/[0.5] flex rounded-[0.7rem] flex-wrap max-[850px]:flex-col justify-center">
-      <div className="flex bg-[#C7FA50] rounded-[0.7rem] min-[850px]:h-full max-[850px]:w-full w-[13.4rem] h-full flex-col gap-[0rem] place-content-center justify-center items-center">
+    <div className="bg-[#C7FA50]/[0.5] max-[850px]:w-[33rem] flex rounded-[0.7rem] flex-wrap max-[850px]:flex-col justify-center  max-[850px]:items-center">
+      <div className="flex bg-[#C7FA50] rounded-[0.7rem]  max-[850px]:w-full w-[13.4rem]  max-[850px]:py-[2rem] flex-col gap-[0rem] place-content-center justify-center items-center">
         <span className="text-[#5B5B5B] text-[1.6rem] font-[600]">
           Solix fee
         </span>
         <span className="text-[#5B5B5B] text-[1.4rem] font-[500]">$0</span>
       </div>
-      <div className="flex h-full py-[2rem] ">
+      <div className="flex h-full min-[851px]:py-[2rem] ">
         <div className="flex flex-col max-[850px]:py-[4rem]  justify-center items-center max-[850px]:border-r-[0px] max-[850px]:border-b-[1px] border-r-[1px] border-[#5B5B5B]">
           <span className="text-[#5B5B5B] text-[1.6rem] font-[600]">
             Save $50 fee
@@ -20,8 +20,8 @@ const FeeCard = () => {
           </span>
         </div>
       </div>
-      <div className="flex h-full py-[2rem]">
-        <div className="flex flex-col  justify-center items-center border-r-[1px]  max-[850px]:border-r-[0px] max-[850px]:border-b-[1px] border-[#5B5B5B]">
+      <div className="flex h-full min-[851px]:py-[2rem]">
+        <div className="flex flex-col max-[850px]:py-[4rem] justify-center items-center border-r-[1px]  max-[850px]:border-r-[0px] max-[850px]:border-b-[1px] border-[#5B5B5B]">
           <span className="text-[#5B5B5B] text-[1.6rem] font-[600]">
             Save $50 fee
           </span>
@@ -30,8 +30,8 @@ const FeeCard = () => {
           </span>
         </div>
       </div>
-      <div className="flex h-full py-[2rem]">
-        <div className="flex flex-col  justify-center items-center">
+      <div className="flex h-full min-[851px]:py-[2rem]">
+        <div className="flex flex-col max-[850px]:py-[4rem] justify-center items-center">
           <span className="text-[#5B5B5B] text-[1.6rem] font-[600]">
             Save $50 fee
           </span>
